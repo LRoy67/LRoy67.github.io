1f3eb7bc2d2c841c09a9d67f8a85bdb034bd9b05
LRoy67 <2503548@spsd.sk.ca>
2025-09-12 14:09:21 -0600

    Update sketch.js

--- 02 User Events/sketch.js
+++ 02 User Events/sketch.js
@@ -4,17 +4,53 @@
 
 // Global Variables here...
 let circleColor = false;
+let currentColor = "white"
 // decloration  initilization
-
+let x;
+let y = 300;
+let tSize = 50; // for text font size
 
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  x = width/2
 }
 
 function draw() {
-
   background(220);
-  challange(); // coordinate system challange
+  challenge(); // coordinate system challange
+  movement();
+  rect(x, y, 60, 30);
+  mouseReport();
+}
+
+function mouseReport(){
+// inspect some of the built-ins(system variables)
+// for working with the mouse
+
+fill(0);
+let src = mouseX + ", " + mouseY + ", " + mouseIsPressed + ", " + mouseButton;
+textSize(tSize);
+text(src, mouseX, mouseY);
+
+}
+
+function mousePressed(){
+// function automatically called ONCE per mouse click
+tSize = random(10, 80)
+}
+
+function movement(){
+  // Check for keyboard  presses for each frame
+  // and move the rectangle accordingly
+
+  //if(keyCode === RIGHT_ARROW && keyIsPressed) x += 5
+  //else if(keyCode === LEFT_ARROW && keyIsPressed) x -= 5
+
+  if (keyIsDown(RIGHT_ARROW)) x += 5;
+  if (keyIsDown(LEFT_ARROW)) x -= 5;
+  if (keyIsDown(UP_ARROW)) y -= 5;
+  if (keyIsDown(DOWN_ARROW)) y += 5;
 }
 
 
@@ -23,17 +59,21 @@ function keyPressed(){
   //gets automatically called anytime a
   // keyboard button is pressed.
 
+  if(keyCode === LEFT_ARROW) x = x - 10
+
   print("key was pressed")
+  if(key === "g") currentColor = "green";
+  else if(keyCode === CONTROL) currentColor = "aqua";
   circleColor = !circleColor;
 }
 
-function challange(){
+function challenge(){
   // Draw 5 hollow circles, in 4 corners
   // and in the center 50px in diameter
 noFill();
 
 if (circleColor === true){ // circleColor ===
-fill(255, 0, 0)}
+fill(currentColor)}
 
 
 // 5 circles
@@ -43,3 +83,12 @@ circle(0, 400, 50)
 circle(0, 0, 50)
 circle(400, 400, 50)
 }
+
+
+
+
+
+
+
+
+
